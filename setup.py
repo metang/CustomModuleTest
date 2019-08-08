7ec7647e3988c48870d7975d5a05c5b09576d3bb
Meng Tang <metang@microsoft.com>
2019-08-08 10:03:00 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="Movie Lens Data",
+    version="0.0.1",
+    description="Movie Lens Data for Recommender test",
+    packages=["script"],
+    author="Meng Tang",
+    license="MIT",
+    include_package_data=True,
+)
