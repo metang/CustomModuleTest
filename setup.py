566bdd545c399ac88660945854e9fb3fde9f0670
Meng Tang <metang@microsoft.com>
2019-08-08 10:41:04 +0800

    add reco_util

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="Movie Lens Data",
     version="0.0.1",
     description="Movie Lens Data for Recommender test",
-    packages=["script"],
+    packages=["script", "reco_utils"],
     author="Meng Tang",
     license="MIT",
     include_package_data=True,
